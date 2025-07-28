80be05bc2df05eeb0d91be6287f9edb503597416
yiminghub2024 <482890@qq.com>
2025-07-28 14:00:21 +0800

    Delete backend directory

--- backend/sql/add_vpc_fields.sql
+++
@@ -1,3 +0,0 @@
--- 在cloud表中添加新的VPC相关字段
-ALTER TABLE `cloud` ADD COLUMN `vpcid` VARCHAR(255) COMMENT 'VPC ID标识符' AFTER `vpc`;
-ALTER TABLE `cloud` ADD COLUMN `vpccidr` VARCHAR(100) COMMENT 'VPC CIDR地址块' AFTER `vpcid`; 
